f55442172038e720d6450009a97c23bcf999b5d5
SantaRenata <renatasanta2002@gmail.com>
2022-02-22 08:36:23 +0100

    Kezdes

--- trapter.java
+++ trapter.java
@@ -7,3 +7,14 @@
 * Github: https://github.com/SantaRenata/
 * Licenc: GNU GPL
 */
+
+import java.util.Scanner;
+class trapter {
+		public static void main (String[] args) {
+			Scanner scan = new Scanner(System.in);
+			
+			System.out.println("Sánta Renáta Diána, Szoft I N, 2022-02-22");
+			System.out.println("Feladat 0316");
+		
+		}
+}
