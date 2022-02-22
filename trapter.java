94a410a7cd7a84299727a650c3d2431e1cf2566a
SantaRenata <renatasanta2002@gmail.com>
2022-02-22 08:47:33 +0100

    A feladat kesz

--- trapter.java
+++ trapter.java
@@ -13,8 +13,24 @@ class trapter {
 		public static void main (String[] args) {
 			Scanner scan = new Scanner(System.in);
 			
-			System.out.println("Sánta Renáta Diána, Szoft I N, 2022-02-22");
+			System.out.println("Santa Renata Diana, Szoft I N, 2022-02-22");
 			System.out.println("Feladat 0316");
+			
+			
+			System.out.print("a oldal: ");
+			double a = scan.nextDouble();
+			
+			System.out.print("b oldal: ");
+			double b = scan.nextDouble();
+			
+			System.out.print("c oldal: ");
+			double c = scan.nextDouble();
+			
+			System.out.print("d oldal: ");
+			double d = scan.nextDouble();
+			
+			double terulet = (a+c)/4*(a-c)*Math.sqrt((a+b-c+d)*(a-b-c+d)*(a+b-c-d)*(-a+b+c+d));
+			System.out.print("Terulet: " + terulet);
 		
 		}
 }
