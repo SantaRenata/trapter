11c207bdfa7b83896efcc8708b53941c4ad5dbb1
SantaRenata <renatasanta2002@gmail.com>
2022-02-22 08:32:43 +0100

    Forraskod fejlec

---
+++ trapter.java
@@ -0,0 +1,9 @@
+/*
+* File: trapter.java
+* Author: Sánta Renáta Diána
+* Copyright: 2022, Sánta Renáta Diána
+* Group: Szoft I N
+* Date: 2022-02-22
+* Github: https://github.com/SantaRenata/
+* Licenc: GNU GPL
+*/
